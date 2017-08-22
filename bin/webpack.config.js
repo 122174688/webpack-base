61c40dd91713d6f65b170de3a67f5b34b82d07e5
linfei <linfei@gomeplus.com>
2017-08-22 11:18:47 +0800

    add files

---
+++ bin/webpack.config.js
@@ -0,0 +1,149 @@
+﻿var path = require('path');
+var fs = require('fs');
+var webpack = require('webpack');
+
+var paths = require('./paths.js');				//简化路径
+var alias = require('./alias.js');				//别名配置
+
+
+
+var env = require('./env.js');					//开发环境
+var pathRewrite = require('./pathRewrite.js');	//路径替换
+
+var getEntry  = require('./getEntry.js');		//处理js
+var ExtractTextPlugin = require("extract-text-webpack-plugin");	//处理css
+var HtmlWebpackPlugin = require('html-webpack-plugin');  //处理pug
+var dealHtml = require('./dealHtml.js');
+
+var TransferWebpackPlugin = require('transfer-webpack-plugin');
+
+//var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+
+var src = paths.src;
+var dist = paths.dist;
+
+
+var exp = {
+//module.exports = {
+	entry:getEntry(),
+	//entry:'src/js/page/test/index.js',
+    output: {
+			
+		path:dist,
+
+		filename: 'js/conf/[name].js',
+		chunkFilename: "js/conf/[name].[chunkHash:8].js"
+    },
+    devServer:{ 
+		publicPath: '/dist',
+		host:env.host,	//域名
+        port:env.port,	//端口
+		proxy:{
+			//开发环境
+			'/CDN':{
+				target:'/',							//替换的主路径
+                secure: false,						
+                pathRewrite: pathRewrite.dev		//路径替换
+			},
+			//仿真环境
+			'/m/pc/dist/**': {
+                target: 'https://js-pre.meixincdn.com',	//https 的target 需要加上https 头
+                secure: false,
+                pathRewrite: pathRewrite.prd
+            }
+		}
+    },
+	resolve: {
+        alias: alias
+    },
+	module:{
+		loaders:[
+			{
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                } //将react编译成js文件
+            },
+			//sass
+			/*
+			{
+                test: /module\\.*?\.scss$/,
+				//exclude: '/node_modules/',	//不执行
+				include:paths.scss,
+                loader: ExtractTextPlugin.extract('css-loader!sass-loader',{
+					publicPath: "/"
+				}) //这里用了样式分离出来的插件
+            },
+			*/
+			{
+				test: /\.scss$/,     
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader!sass-loader",
+					publicPath:'dist'
+				})
+			},
+			//pug
+			{
+				test: /\.pug$/,
+				//exclude: '/node_modules/',	//不执行
+				include:paths.pug,
+				loader:"pug-loader",
+				query:{
+					pretty:true        //防止压缩
+				}
+			},
+			//tpl
+			{
+				test: /\.tpl$/,
+				include: paths.page,
+				loader: 'tmodjs-loader'
+			},
+			//images
+			{
+				test: /\.(png|jpg|gif)$/,
+				include:paths.images,
+				//exclude: '/node_modules/',	//不执行
+				loader: 'url-loader',
+				options:{
+					//相对路径： 不生成image
+					limit:1,
+					publicPath:'',	//  		重新指定目录
+					name: '[name].[ext]',
+					context:paths.src,
+					useRelativePath:true	//相对路径写法
+				}
+			 }
+		]
+	},
+	
+	plugins:[
+		new ExtractTextPlugin('css/module/[name].css'),
+		new TransferWebpackPlugin([{
+            from: 'src/images',
+            to: 'images'
+        }]),
+		new webpack.optimize.CommonsChunkPlugin({
+            name: ['vendor','js/main'],
+            filename: 'js/conf/[name].js',
+            minChunks: Infinity
+        }),
+		new webpack.ProvidePlugin({
+            React: "react",
+            ReactDOM: "react-dom"
+        })
+		/*
+		new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        })
+		*/
+	]
+}
+
+dealHtml(exp.plugins);
+	
+module.exports = exp;
---
+++ src/js/page/test/index.js
@@ -0,0 +1,10 @@
+//var React = require("react");
+//var ReactDOM = require('react-dom');
+var Template = require('./reactTest.js');
+
+
+ReactDOM.render(
+  <Template title={6666}/>,
+  document.getElementById('demo')
+);
+
